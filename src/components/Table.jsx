954b6861bd24679c6e8f0e1541c0489fa470debb
Stefano Miceli <mic.ninja@gmail.com>
2025-02-10 10:51:48 +0100

    refactors Table to be a functional component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,53 +1,50 @@
-import { Component } from "react";
 import { ListGroup } from "react-bootstrap";
 
-class Table extends Component {
+const Table = (props) => {
   //   state = {
   //     selected: ""
   //   };
 
-  checkSelected = (tablePos) => (this.props.selected === tablePos ? `table-item-selected` : "");
+  const checkSelected = (tablePos) => (props.selected === tablePos ? `table-item-selected` : "");
 
-  render() {
-    console.log("RENDER Table");
-    return (
-      <>
-        <ListGroup>
-          <ListGroup.Item
-            //   onClick={() => this.setState({ selected: "Uno" })}
-            onClick={() => this.props.changeSelected("Uno")}
-            className={this.checkSelected("Uno")}
-          >
-            Uno
-          </ListGroup.Item>
-          <ListGroup.Item
-            //   onClick={() => this.setState({ selected: "Due" })}
-            onClick={() => this.props.changeSelected("Due")}
-            className={this.checkSelected("Due")}
-          >
-            Due
-          </ListGroup.Item>
-          <ListGroup.Item
-            //   onClick={() => this.setState({ selected: "Tre" })}
-            onClick={() => this.props.changeSelected("Tre")}
-            className={this.checkSelected("Tre")}
-          >
-            Tre
-          </ListGroup.Item>
-          <ListGroup.Item
-            //   onClick={() => this.setState({ selected: "Quattro" })}
-            onClick={() => this.props.changeSelected("Quattro")}
-            className={this.checkSelected("Quattro")}
-          >
-            Quattro
-          </ListGroup.Item>
-        </ListGroup>
-        <p>
-          L&apos;elemento attualmente selezionato è <strong>{this.props.selected || "Nessuno"}</strong>
-        </p>
-      </>
-    );
-  }
-}
+  console.log("RENDER Table");
+  return (
+    <>
+      <ListGroup>
+        <ListGroup.Item
+          //   onClick={() => this.setState({ selected: "Uno" })}
+          onClick={() => props.changeSelected("Uno")}
+          className={checkSelected("Uno")}
+        >
+          Uno
+        </ListGroup.Item>
+        <ListGroup.Item
+          //   onClick={() => this.setState({ selected: "Due" })}
+          onClick={() => props.changeSelected("Due")}
+          className={checkSelected("Due")}
+        >
+          Due
+        </ListGroup.Item>
+        <ListGroup.Item
+          //   onClick={() => this.setState({ selected: "Tre" })}
+          onClick={() => props.changeSelected("Tre")}
+          className={checkSelected("Tre")}
+        >
+          Tre
+        </ListGroup.Item>
+        <ListGroup.Item
+          //   onClick={() => this.setState({ selected: "Quattro" })}
+          onClick={() => props.changeSelected("Quattro")}
+          className={checkSelected("Quattro")}
+        >
+          Quattro
+        </ListGroup.Item>
+      </ListGroup>
+      <p>
+        L&apos;elemento attualmente selezionato è <strong>{props.selected || "Nessuno"}</strong>
+      </p>
+    </>
+  );
+};
 
 export default Table;
